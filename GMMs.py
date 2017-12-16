a5ac4c43e566d1160b74013c9602d95741021494
MaherSaleem <the-maher@live.com>
2017-12-16 17:16:48 +0200

    commented gmm plotting code since it may cause error while training

--- GMMs.py
+++ GMMs.py
@@ -75,51 +75,50 @@ def calcaulteGMMForEachClass(X, start=1, end=7, classIndex=0) -> mixture.Gaussia
     bic = np.array(bic)
     color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
                                   'darkorange'])
-    clf = best_gmm
-    bars = []
     print("best number of components for this class is : ", best_component)
     print("best covariance type is for this class is : ", best_cv_type)
-    print("==="*20)
-    # Plot the BIC scores
-    spl = plt.subplot(2, 1, 1)
-    for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
-        xpos = np.array(n_components_range) + .2 * (i - 2)
-        bars.append(plt.bar(xpos, bic[i * len(n_components_range):
-        (i + 1) * len(n_components_range)],
-                            width=.2, color=color))
-    plt.xticks(n_components_range)
-    plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
-    plt.title('BIC score per model')
-    xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 + \
-           .2 * np.floor(bic.argmin() / len(n_components_range))
-    plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
-    spl.set_xlabel('Number of components')
-    spl.legend([b[0] for b in bars], cv_types)
-
+    clf = best_gmm
+    # bars = []
+    # print("==="*20)
+    # # Plot the BIC scores
+    # spl = plt.subplot(2, 1, 1)
+    # for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    #     xpos = np.array(n_components_range) + .2 * (i - 2)
+    #     bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+    #     (i + 1) * len(n_components_range)],
+    #                         width=.2, color=color))
+    # plt.xticks(n_components_range)
+    # plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+    # plt.title('BIC score per model')
+    # xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 + \
+    #        .2 * np.floor(bic.argmin() / len(n_components_range))
+    # plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+    # spl.set_xlabel('Number of components')
+    # spl.legend([b[0] for b in bars], cv_types)
     # Plot the winner
-    splot = plt.subplot(2, 1, 2)
-    Y_ = clf.predict(X)
-    for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_,
-                                               color_iter)):
-        v, w = linalg.eigh(cov)
-        if not np.any(Y_ == i):
-            continue
-        plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], .8, color=color)
-
-        # Plot an ellipse to show the Gaussian component
-        angle = np.arctan2(w[0][1], w[0][0])
-        angle = 180. * angle / np.pi  # convert to degrees
-        v = 2. * np.sqrt(2.) * np.sqrt(v)
-        ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
-        ell.set_clip_box(splot.bbox)
-        ell.set_alpha(.5)
-        splot.add_artist(ell)
-        # h = plt.subplot(1, n_components // 1, 1 + 0)
-        # make_ellipses(gmm, h)
-
-    plt.xticks(())
-    plt.yticks(())
-    plt.title('Selected GMM: ' + best_cv_type + ' model, ' + str(best_component) + ' components')
-    plt.subplots_adjust(hspace=.35, bottom=.02)
-    # plt.show()
+    # splot = plt.subplot(2, 1, 2)
+    # Y_ = clf.predict(X)
+    # for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_,
+    #                                            color_iter)):
+    #     v, w = linalg.eigh(cov)
+    #     if not np.any(Y_ == i):
+    #         continue
+    #     plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], .8, color=color)
+    #
+    #     # Plot an ellipse to show the Gaussian component
+    #     angle = np.arctan2(w[0][1], w[0][0])
+    #     angle = 180. * angle / np.pi  # convert to degrees
+    #     v = 2. * np.sqrt(2.) * np.sqrt(v)
+    #     ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
+    #     ell.set_clip_box(splot.bbox)
+    #     ell.set_alpha(.5)
+    #     splot.add_artist(ell)
+    #     # h = plt.subplot(1, n_components // 1, 1 + 0)
+    #     # make_ellipses(gmm, h)
+    #
+    # plt.xticks(())
+    # plt.yticks(())
+    # plt.title('Selected GMM: ' + best_cv_type + ' model, ' + str(best_component) + ' components')
+    # plt.subplots_adjust(hspace=.35, bottom=.02)
+    # # plt.show()
     return best_gmm
